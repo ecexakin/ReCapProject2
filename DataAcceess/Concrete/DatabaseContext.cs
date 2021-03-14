65741d6f4928b85a0ce4b48dce8b5fa08151f42b
Ece Akın <akinecee3535@hotmail.com>
2021-03-14 14:16:50 +0300

    Add project files.

---
+++ DataAcceess/Concrete/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    // context : db tabloları ile class tablolarını ilişkilendirmek
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-9AAHPK3;Database=CarRentalProject;Trusted_Connection=true");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+    }
+
+}
---
+++ DataAcceess/Concrete/EfCarDal.cs
@@ -0,0 +1,33 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, DatabaseContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (DatabaseContext context = new DatabaseContext())
+            {
+                var result = from c in context.Cars
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             select new CarDetailDto
+                             {
+                                 CarName = c.CarName,
+                                 BrandName = b.BrandName,
+                                 DailyPrice = c.DailyPrice
+                             };
+                return result.ToList();
+
+
+            }
+        }
+    }
+}
